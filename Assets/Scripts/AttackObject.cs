1dc66d88c2c4a80105a6b63500e32ab3c01fe6c3
uoo38 <plus.wisteria38@gmail.com>
2016-06-20 14:06:31 +0900

    add AttackObject & EmitAttackObject scripts

---
+++ Assets/Scripts/AttackObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackObject : MonoBehaviour {
+
+  public float lifetime = 1.5f;
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  void OnCollisionEnter(Collision otherObj) {
+    // 何かしらに衝突した場合 lifetime 時間後に消滅
+    Destroy(this.gameObject, lifetime);
+  }
+}
---
+++ Assets/Scripts/EmitAttackObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmitAttackObject : MonoBehaviour {
+
+  public GameObject attackObjectPrefab;
+  public float initialSpeed;
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if ( Input.GetMouseButtonDown(0) ) {
+      // attackObject を複製
+      GameObject attackObject = (GameObject)Instantiate (attackObjectPrefab, transform.position, transform.rotation);
+      // 左クリックされた座標から単位ベクトルを計算
+      var screenMousePosition = Input.mousePosition;
+      screenMousePosition.z = 10f;
+      // スクリーン座標からワールド座標への変換
+      Vector3 direction = Camera.main.ScreenToWorldPoint (screenMousePosition);
+      // スクリーン座標にはzがないため追加
+      // direction.z = target.transform.position.z - transform.position.z;
+      direction.z = -transform.position.z;
+      // attackObject に初速を追加
+      attackObject.GetComponent<Rigidbody> ().velocity = direction * initialSpeed;
+    }
+  }
+}
