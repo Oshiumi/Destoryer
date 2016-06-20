086db371b6f93192052b73b55f5138ea6c964c37
aoikato <cemsy5cmu34bsmhgpisx@docomo.ne.jp>
2016-06-20 16:00:54 +0900

    pointgetを作りました

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public int sum = 0;
+
+	// Use this for initialization
+	void Start () {
+		sum = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/scripts/PointGet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointGet : MonoBehaviour {
+    public GameManager gm;
+    public GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gm = gameManager.GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnCollisionEnter()
+    {
+        gm.sum += 100;
+    }
+}
