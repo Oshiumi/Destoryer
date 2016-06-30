a8c6c6d16beb574286f4483a5d21660eb901f316
aoikato <cemsy5cmu34bsmhgpisx@docomo.ne.jp>
2016-06-30 14:53:09 +0900

    爆発オブジェクトのプレハブをインスタンス化するスクリプトを作りました

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+	public Transform explosion;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Attackobj")
+			Instantiate (explosion, collision.transform.position, Quaternion.identity);			
+	}
+}
